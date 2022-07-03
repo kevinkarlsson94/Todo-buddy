66f4388285c6a8e93862ca7538b63b82f15e0a7f
Kevin Karlsson <kevin.karlsson@forefront.se>
2022-07-03 23:37:21 +0200

    refactor keyboard avoid

--- src/TodoList/TodoList.tsx
+++ src/TodoList/TodoList.tsx
@@ -1,12 +1,17 @@
-import React, { useReducer, useState } from 'react'
+import React, { useReducer, useRef, useState } from 'react'
 import { TodoListContext } from './TodoList.context'
 import { initialState, todosReducer } from './TodoList.reducer'
 import { Header, AddUpdateTodo, TodoItem } from './Fragments'
 import { ITodo } from './TodoList.types'
-import { Dimensions, View, StyleSheet, ScrollView, Text } from 'react-native'
+import {
+  Dimensions,
+  View,
+  StyleSheet,
+  ScrollView,
+  KeyboardAvoidingView,
+} from 'react-native'
 import { colors } from '../Variables/colors'
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
-import { useKeyboardHeight } from '../Hooks/useKeyboardHeight'
+
 export const TodoList = () => {
   // I decided to use useReducer to keep and manage the state.
   const [todoAppState, dispatch] = useReducer(todosReducer, initialState)
@@ -15,8 +20,8 @@ export const TodoList = () => {
   // I decided to lift this state since it's used in 2 components (AddUpdateTodo and TodoItem) and pass it to the TodoListContext to prevent prop-drilling
   const [editingItem, setEditingItem] = useState<ITodo | null>()
 
-  const { keyboardHeight } = useKeyboardHeight()
   const windowHeight = Dimensions.get('window').height
+  const scrollViewRef = useRef<ScrollView>(null)
 
   return (
     // Since some state/functionalities have to be shared between components I decided to use the react context api for this.
@@ -30,18 +35,20 @@ export const TodoList = () => {
         },
       }}
     >
-      {/* had to use KeyboardAwareScrollView to make sure the keyboard is always on the screen if keyboard is expanded */}
-      <KeyboardAwareScrollView
-        contentContainerStyle={{
-          height: windowHeight - keyboardHeight,
-        }}
-        enableAutomaticScroll={false}
-        enableOnAndroid={true}
-        keyboardShouldPersistTaps="always"
+      {/* KeyboardAvoidingView to prevent keyboard overflowing the input when toggled */}
+      <KeyboardAvoidingView
+        behavior={'height'}
+        style={{ height: windowHeight }}
       >
         <View style={[styles.container, { height: windowHeight }]}>
           <Header title="TodoBuddie" />
-          <ScrollView>
+          <ScrollView
+            ref={scrollViewRef}
+            onContentSizeChange={() =>
+              scrollViewRef.current &&
+              scrollViewRef.current.scrollToEnd({ animated: true })
+            } // scroll to bottom if adding elements
+          >
             {todoAppState.todos.map((todo, index) => (
               <TodoItem index={index} todo={todo} key={index} />
             ))}
@@ -50,7 +57,7 @@ export const TodoList = () => {
           </ScrollView>
           <AddUpdateTodo />
         </View>
-      </KeyboardAwareScrollView>
+      </KeyboardAvoidingView>
     </TodoListContext.Provider>
   )
 }
